abb0fb8b6694a4f2c47115382ea904f46fdec4c8
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-11-02 15:31:25 +0100

    Disable blas backend for now.

--- tests/algorithms/power_method.cpp
+++ tests/algorithms/power_method.cpp
@@ -35,37 +35,38 @@ TEMPLATE_TEST_CASE("PowerMethod EigenDense backend", "[algorithms][eigen]",
             solver.eigenvalues().cwiseAbs().maxCoeff());
 }
 
-#ifdef ITESOL_TEST_USE_BLAS
+// #ifdef ITESOL_TEST_USE_BLAS
 
-TEMPLATE_TEST_CASE("PowerMethod Blas backend", "[algorithms][blas]", double) {
-    using Backend = itesol::backends::Blas<TestType>;
-    using Algorithm = itesol::algorithms::PowerMethod<Backend>;
+// TEMPLATE_TEST_CASE("PowerMethod Blas backend", "[algorithms][blas]", double)
+// {
+//     using Backend = itesol::backends::Blas<TestType>;
+//     using Algorithm = itesol::algorithms::PowerMethod<Backend>;
 
-    const int dimension = 64;
-    const typename Backend::RealScalar tolerance = 1e-6;
+//     const int dimension = 64;
+//     const typename Backend::RealScalar tolerance = 1e-6;
 
-    Backend backend{};
+//     Backend backend{};
 
-    auto matrix = backend.create_matrix(dimension, dimension);
-    itesol::test_matrices::initialize_random_symmetric<Backend>(matrix);
+//     auto matrix = backend.create_matrix(dimension, dimension);
+//     itesol::test_matrices::initialize_random_symmetric<Backend>(matrix);
 
-    Algorithm algorithm(dimension, backend, 10000, tolerance);
-    algorithm.compute(backend.make_linear_operator(matrix));
+//     Algorithm algorithm(dimension, backend, 10000, tolerance);
+//     algorithm.compute(backend.make_linear_operator(matrix));
 
-    REQUIRE(algorithm.is_converged());
+//     REQUIRE(algorithm.is_converged());
 
-    Eigen::Matrix<TestType, Eigen::Dynamic, Eigen::Dynamic> eigen_matrix(
-        dimension, dimension);
-    for (int i = 0; i < dimension; ++i) {
-        for (int j = 0; j < dimension; ++j) {
-            eigen_matrix(i, j) = matrix(i, j);
-        }
-    }
-    Eigen::SelfAdjointEigenSolver<decltype(eigen_matrix)> solver;
-    solver.compute(eigen_matrix);
+//     Eigen::Matrix<TestType, Eigen::Dynamic, Eigen::Dynamic> eigen_matrix(
+//         dimension, dimension);
+//     for (int i = 0; i < dimension; ++i) {
+//         for (int j = 0; j < dimension; ++j) {
+//             eigen_matrix(i, j) = matrix(i, j);
+//         }
+//     }
+//     Eigen::SelfAdjointEigenSolver<decltype(eigen_matrix)> solver;
+//     solver.compute(eigen_matrix);
 
-    REQUIRE(Catch::Approx(algorithm.get_eigenvalue()) ==
-            solver.eigenvalues().cwiseAbs().maxCoeff());
-}
+//     REQUIRE(Catch::Approx(algorithm.get_eigenvalue()) ==
+//             solver.eigenvalues().cwiseAbs().maxCoeff());
+// }
 
-#endif
+// #endif
